208b1381882fd1abea22acabf9854ae4a6e353da
dindaapermatasari <dindapermatasari020@gmail.com>
2024-09-17 19:01:48 +0700

    initial commit

--- board/__init__.py
+++ board/__init__.py
@@ -1,12 +1,8 @@
 from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def home():
-    return "Hello, World!"
+from board import pages
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(pages.bp)
+    return app
