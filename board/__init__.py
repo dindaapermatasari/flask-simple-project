89c5bbca1155dca1335c4d78e09cf3a154b23260
dindaapermatasari <dindapermatasari020@gmail.com>
2024-09-18 18:27:32 +0700

    modified file __init__.py

--- board/__init__.py
+++ board/__init__.py
@@ -10,6 +10,7 @@ def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(pages.bp)
-    app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)
+    print(f"Current Environment: {os.getenv('ENVIRONMENT')}")
+    print(f"Using Database: {app.config.get('DATABASE')}")
     return app
