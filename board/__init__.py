0bbdbc65c217c3298a5f534235fcaa0a13b665c6
dindaapermatasari <dindapermatasari020@gmail.com>
2024-09-18 18:24:58 +0700

    modified file __init__.py

--- board/__init__.py
+++ board/__init__.py
@@ -1,10 +1,15 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from board import pages, posts
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
 
+    app.register_blueprint(pages.bp)
     app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)
     return app
